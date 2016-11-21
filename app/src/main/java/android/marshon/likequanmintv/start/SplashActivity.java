d726c98f64fae04457397b61ae80e1d8e502ca9f
陈成印 <itmarshon@163.com>
2016-11-21 15:03:49 +0800

    初始化

---
+++ app/src/main/java/android/marshon/likequanmintv/start/SplashActivity.java
@@ -0,0 +1,86 @@
+package android.marshon.likequanmintv.start;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.marshon.likequanmintv.R;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Marshon.Chen on 2016/6/1.
+ * DESC:
+ */
+public class SplashActivity extends AppCompatActivity implements Animator.AnimatorListener {
+
+
+    private View rootView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        start();
+    }
+
+    private void start() {
+        rootView = findViewById(R.id.main_root);
+        final View viewById = findViewById(R.id.tt);
+        ValueAnimator valueAnimator=ValueAnimator.ofFloat(0.3f,1.0f);
+        valueAnimator.setDuration(1200);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float alpha = (float) animation.getAnimatedValue();
+                rootView.setAlpha(alpha);
+                viewById.setScaleX(alpha);
+                viewById.setScaleY(alpha);
+
+            }
+        });
+        valueAnimator.addListener(this);
+        valueAnimator.start();
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String firstrun = "";
+        if (TextUtils.isEmpty(firstrun)){
+            Intent intent = new Intent(this,GuideActivity.class);
+            startActivity(intent);
+            finish();
+        }else{
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
---
+++ app/src/main/res/layout/activity_splash.xml
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+              android:orientation="vertical"
+              android:id="@+id/main_root"
+              android:layout_width="match_parent"
+              android:layout_height="match_parent">
+    <LinearLayout
+        android:id="@+id/tt"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_centerInParent="true"
+        android:orientation="vertical">
+        <ImageView
+            android:layout_width="150dp"
+            android:layout_height="150dp"
+            android:src="@mipmap/ic_launcher"/>
+        <TextView
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:layout_marginTop="10dp"
+            android:layout_gravity="center_horizontal"
+            android:textSize="30sp"
+            
+            android:text="666"/>
+   
+    </LinearLayout>
+</RelativeLayout>
